7340c8d95d31ddb08838cc77a3b694ed57459c11
Gianluca Nitti <gianluca.nitti96@gmail.com>
2015-11-03 20:29:38 +0100

    Added link in the about dialog; improved layouts.

--- main.c
+++ main.c
@@ -2,12 +2,22 @@
 #include <stdint.h>
 #include <iup.h>
 
+#include <stdio.h>
+
 void led_load (void);
 
 Ihandle *mainDlg, *aboutDlg;
 
-void about(){
+void main_about(){
 	IupPopup(aboutDlg, IUP_CENTER, IUP_CENTER); //Shows the "about" window
+}
+
+void about_iupWeb(){
+	IupHelp(IupGetAttribute(IupGetHandle("iupLink"), "URL"));
+}
+
+void about_close(){
+	IupHide(aboutDlg);
 }
 
 int main(int argc, char **argv)
@@ -17,7 +27,12 @@ int main(int argc, char **argv)
 	mainDlg = IupGetHandle("mainDlg"); //Get the window named "mainDlg"
 	aboutDlg = IupGetHandle("aboutDlg"); //Get the window named "aboutDlg"
 	IupShowXY(mainDlg, IUP_CENTER, IUP_CENTER); //Shows the window
-	IupSetCallback(IupGetHandle("about"), "ACTION", (Icallback)about); //Associates the click action of the button "about" with the "about" function.
+
+	IupSetAttribute(NULL, "HELPAPP", "xdg-open");
+	IupSetCallback(IupGetHandle("about"), "ACTION", (Icallback)main_about); //Callback: Associates the click action of the button "about" with the "about" function.
+	IupSetCallback(IupGetHandle("iupLink"), "ACTION", (Icallback)about_iupWeb); //Callback for the about link
+	IupSetCallback(IupGetHandle("aboutClose"), "ACTION", (Icallback)about_close); //Callback for the about dialog close button
+
   IupMainLoop();
   IupClose();
   return EXIT_SUCCESS;
